60164d982c1650f479433a350779211da6e9aed7
Linda <lindathehuang@gmail.com>
2020-03-30 00:28:03 -0700

    Updated links and nav
    nav styles and link redirects

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 //------------------------------------------------------------------------------
-$(document).ready(function(){
-  $(".navinvolvement").on("mouseenter", function(){//display hover nav
-    $(".navInvolvementSections").slideToggle();
-  });
+$(document).ready(function() {
+    $(".subnav").hover(
+        function() {
+            $(this).addClass("showSubnav");
+        },
+        function() {
+            $(this).removeClass("showSubnav");
+        },
+        function() {
+            $("showSubnav").slideToggle()
+        }
+    );
+
+    $(".subnav div, .subnav a").hover(
+        function() {
+            $(".subnav a").css({
+                "border-bottom": "3px solid rgba(120, 155, 183, 1)"
+            });
+
+            $(".subnav div a").css({
+                "border-bottom": "0px",
+            });
+        },
+        function() {
+            $(".subnav a").css({
+                "border-bottom": "0px"
+            });
+        }
+    );
 });
 
-window.onload = function(){
-  //document.getElementById("intro").style = "background:green";
+window.onload = function() {
+    //document.getElementById("intro").style = "background:green";
 }
